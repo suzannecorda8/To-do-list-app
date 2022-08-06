af52b9062400899c2621b8711bbbc2c9c33589a3
suzannecorda8 <103393836+suzannecorda8@users.noreply.github.com>
2022-08-06 21:17:48 +0530

    my_first_app

--- models.py
+++ models.py
@@ -2,7 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model) :
-    task = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.task
+    task = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
